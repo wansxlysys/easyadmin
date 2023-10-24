6013728a533993c75e5c8f8e5620b3bc191d23ba
liming <1628883533@qq.com>
2023-10-24 14:06:58 +0800

    增加常用函数

--- application/common.php
+++ application/common.php
@@ -24,3 +24,61 @@ if (!function_exists('array_to_base64')) {
         return base64_encode(json_encode($data));
     }
 }
+
+if (!function_exists('file_prefix')) {
+    /**
+     * 补齐文件域名前缀
+     * @param $filePath
+     * @param bool $toArray
+     * @param string $perfix
+     * @return mixed
+     */
+    function file_prefix($filePath, $toArray = false, $perfix = 'system_domain')
+    {
+        if (empty($filePath)) {
+            return $filePath;
+        }
+
+        $domain = config('system.' . $perfix);
+
+        if (!is_array($filePath)) {
+            $filePath = explode(',', $filePath);
+        }
+
+        foreach ($filePath as $key => $file) {
+            $filePath[$key] = $domain . $file;
+        }
+
+        return $toArray ? $filePath : implode(',', $filePath);
+    }
+}
+
+if (!function_exists('system_prefix')) {
+    /**
+     * 后端文件域名前缀
+     * @param $filePath
+     * @param bool $toArray
+     * @return mixed
+     */
+    function system_prefix($filePath, $toArray = false)
+    {
+        return file_prefix($filePath, $toArray, 'system_domain');
+    }
+}
+
+if (!function_exists('empty_image')) {
+    /**
+     * 输出展位图片
+     * @param $image
+     * @param $default
+     * @return bool|mixed
+     */
+    function empty_image($image, $default)
+    {
+        if (empty($image)) {
+            return $default;
+        }
+
+        return $image;
+    }
+}

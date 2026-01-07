62e25b111ade7637c69912da3d2a109912509ba7
liming <1628883533@qq.com>
2026-01-07 15:29:56 +0800

    .

--- application/admin/service/SystemUploadService.php
+++ application/admin/service/SystemUploadService.php
@@ -130,9 +130,9 @@ class SystemUploadService extends Service
             /**
              * 检测文件大小
              */
-            $uploadLimit = $this->SystemSettingService->getSystemSettingValue('upload', 'limit');
+            $uploadLimit = intval($this->SystemSettingService->getSystemSettingValue('upload', 'limit'));
 
-            if (intval($uploadLimit) && UploadHelper::fileSizeToMb($params['size']) > $uploadLimit) {
+            if ($uploadLimit && UploadHelper::fileSizeToMb($params['size']) > $uploadLimit) {
                 throw new ServiceException('文件大小超出限制');
             }
 

dbcb8fe66f0a6952782f10f730e42a1a23cefec9
liming <1628883533@qq.com>
2026-01-15 16:40:20 +0800

    .

--- public/static/admin/module/easyAdmin/easyModule.js
+++ public/static/admin/module/easyAdmin/easyModule.js
@@ -3,9 +3,9 @@ layui.define([
     'easyUpload', 'easyMap'
 ], function (exports) {
 
+    const table = layui.table;
     const easyAdmin = layui.easyAdmin;
     const easyLayout = layui.easyLayout;
-    const easyUpload = layui.easyUpload;
 
     /**
      * 移除弹出层透明背景
@@ -16,6 +16,57 @@ layui.define([
      * 暴漏全局
      */
     window.easyLayout = easyLayout;
+
+    /**
+     * 重写表格渲染
+     */
+    const rewriteTableRender = () => {
+
+        const originRender = table.render;
+
+        table.render = function (config) {
+
+            let tableToolbar = null;
+
+            if (jQuery(config.toolbar).length) {
+                tableToolbar = jQuery(config.toolbar).html();
+            }
+
+            if (!tableToolbar || !jQuery(tableToolbar).find('button').length) {
+                delete config.toolbar;
+            }
+
+            config.cols = config.cols.map((rowConfig) => {
+                return rowConfig.filter((colConfig) => {
+                    if (colConfig.dynamic === true) {
+                        let toolbar = null;
+
+                        if (jQuery(colConfig.toolbar).length) {
+                            toolbar = jQuery(colConfig.toolbar).html();
+                        }
+
+                        const length = jQuery(toolbar).filter('button').length ||
+                            jQuery(toolbar).find('button').length;
+
+                        if (toolbar && length) {
+                            colConfig.width = length * 75 + 20;
+                        }
+
+                        return !(!toolbar || !length);
+                    }
+
+                    return true;
+                });
+            });
+
+            return originRender.call(this, config);
+        }
+    }
+
+    /**
+     * 初始化重写
+     */
+    rewriteTableRender();
 
     /**
      * 导出

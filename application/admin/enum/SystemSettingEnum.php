b002236049ea6110920d5cb04dec3c1d447cb4a8
liming <1628883533@qq.com>
2026-01-31 17:24:38 +0800

    .

--- application/admin/enum/SystemSettingEnum.php
+++ application/admin/enum/SystemSettingEnum.php
@@ -7,11 +7,6 @@ namespace app\admin\enum;
 class SystemSettingEnum
 {
     /**
-     * 配置分类
-     */
-    const TYPE_SYSTEM = 'system';
-
-    /**
      * 缓存标识
      */
     const SYSTEM_SETTING = 'system:setting'; // 系统设置
--- application/admin/view/system_index/components.php
+++ application/admin/view/system_index/components.php
@@ -1,6 +1,7 @@
 {extend name="common@layout/layout" /}
 
 {block name="content"}
+{include file="common@layout/breadcrumb" /}
 <div class="layui-fluid layui-content">
 
     <!--    <div class="layui-card">-->

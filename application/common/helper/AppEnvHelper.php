52c9736363df628e60873f730957fd3d2daa5f0c
liming <1628883533@qq.com>
2026-03-05 09:10:11 +0800

    增加环境变量帮助类

---
+++ application/common/helper/AppEnvHelper.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace app\common\helper;
+
+
+use think\facade\Env;
+
+class AppEnvHelper
+{
+    /**
+     * 获取当前环境
+     * @return string
+     */
+    public static function getEnv()
+    {
+        return Env::get('APP_ENV');
+    }
+
+    /**
+     * 是否是测试环境
+     * @return bool
+     */
+    public static function isTest()
+    {
+        return self::getEnv() === 'test';
+    }
+
+    /**
+     * 是否是生产环境
+     * @return bool
+     */
+    public static function isProd()
+    {
+        return self::getEnv() === 'prod';
+    }
+
+    /**
+     * 是否是本地环境
+     * @return bool
+     */
+    public static function isLocal()
+    {
+        return static::getEnv() === 'local';
+    }
+}

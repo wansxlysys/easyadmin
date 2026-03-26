4d0c0d3b7f69488ebfbf16f5172bea07d054aa87
liming <1628883533@qq.com>
2026-03-26 21:13:35 +0800

    优化详情样式

--- application/admin/view/system_login_log/detail.php
+++ application/admin/view/system_login_log/detail.php
@@ -6,42 +6,42 @@
     <div class="layui-card">
         <div class="layui-card-header">{$currentMenu.name}</div>
         <div class="layui-card-body">
-            <div class="easy-info">
-                {assign name="manager" value=":service('SystemManager')->getByManagerId($log.managerId)"}
-                <div class="easy-info-grid easy-info-grid-100 easy-info-grid-center">
+            {assign name="manager" value=":service('SystemManager')->getByManagerId($log.managerId)"}
+            <div class="easy-info layui-row">
+                <div class="easy-info-grid layui-col-xs12 layui-col-md12 easy-info-grid-center">
                     <div class="easy-info-grid-left">管理员头像：</div>
                     <div class="easy-info-grid-right">
                         <img src="{$manager.avatar}" width="35" height="35" class="layui-circle">
                     </div>
                 </div>
-                <div class="easy-info-grid easy-info-grid-25">
+                <div class="easy-info-grid layui-col-xs12 layui-col-md3">
                     <div class="easy-info-grid-left">管理员姓名：</div>
                     <div class="easy-info-grid-right">{$manager.realName}</div>
                 </div>
-                <div class="easy-info-grid easy-info-grid-25">
+                <div class="easy-info-grid layui-col-xs12 layui-col-md3">
                     <div class="easy-info-grid-left">管理员账号：</div>
                     <div class="easy-info-grid-right">{$manager.account}</div>
                 </div>
-                <div class="easy-info-grid easy-info-grid-25">
+                <div class="easy-info-grid layui-col-xs12 layui-col-md3">
                     <div class="easy-info-grid-left">登录IP：</div>
                     <div class="easy-info-grid-right">{$log.loginIp}</div>
                 </div>
-                <div class="easy-info-grid easy-info-grid-25">
+                <div class="easy-info-grid layui-col-xs12 layui-col-md3">
                     <div class="easy-info-grid-left">登录时间：</div>
                     <div class="easy-info-grid-right">{$log.createTime}</div>
                 </div>
-                <div class="easy-info-grid easy-info-grid-25">
+                <div class="easy-info-grid layui-col-xs12 layui-col-md3">
                     <div class="easy-info-grid-left">登录状态：</div>
                     <div class="easy-info-grid-right">
                         {if $log.status == 1}<span class="layui-badge layui-bg-green">登录成功</span>{/if}
                         {if $log.status == 2}<span class="layui-badge">登录失败</span>{/if}
                     </div>
                 </div>
-                <div class="easy-info-grid easy-info-grid-100">
+                <div class="easy-info-grid layui-col-xs12 layui-col-md12">
                     <div class="easy-info-grid-left">用户代理：</div>
                     <div class="easy-info-grid-right">{$log.userAgent}</div>
                 </div>
-                <div class="easy-info-grid easy-info-grid-100">
+                <div class="easy-info-grid layui-col-xs12 layui-col-md12">
                     <div class="easy-info-grid-left">登录详情：</div>
                     <div class="easy-info-grid-right">{$log.message}</div>
                 </div>
--- application/admin/view/system_oper_log/detail.php
+++ application/admin/view/system_oper_log/detail.php
@@ -16,57 +16,57 @@
         <div class="layui-card-body">
             {assign name="menu" value=":service('SystemMenu')->getByMenuId($log.menuId)"}
             {assign name="manager" value=":service('SystemManager')->getByManagerId($log.managerId)"}
-            <div class="easy-info">
-                <div class="easy-info-grid easy-info-grid-100 easy-info-grid-center">
+            <div class="easy-info layui-row">
+                <div class="easy-info-grid easy-info-grid-center layui-col-xs12 layui-col-md12">
                     <div class="easy-info-grid-left">管理员头像：</div>
                     <div class="easy-info-grid-right">
                         <img src="{$manager.avatar}" width="35" height="35" class="layui-circle">
                     </div>
                 </div>
-                <div class="easy-info-grid easy-info-grid-25">
+                <div class="easy-info-grid layui-col-xs12 layui-col-md3">
                     <div class="easy-info-grid-left">管理员姓名：</div>
                     <div class="easy-info-grid-right">{$manager.realName}</div>
                 </div>
-                <div class="easy-info-grid easy-info-grid-25">
+                <div class="easy-info-grid layui-col-xs12 layui-col-md3">
                     <div class="easy-info-grid-left">管理员账号：</div>
                     <div class="easy-info-grid-right">{$manager.account}</div>
                 </div>
-                <div class="easy-info-grid easy-info-grid-25">
+                <div class="easy-info-grid layui-col-xs12 layui-col-md3">
                     <div class="easy-info-grid-left">操作菜单：</div>
                     <div class="easy-info-grid-right">{$menu.name}</div>
                 </div>
-                <div class="easy-info-grid easy-info-grid-25">
+                <div class="easy-info-grid layui-col-xs12 layui-col-md3">
                     <div class="easy-info-grid-left">操作状态：</div>
                     <div class="easy-info-grid-right">
                         {if $log.status == 1}<span class="layui-badge layui-bg-green">操作成功</span>{/if}
                         {if $log.status == 2}<span class="layui-badge">操作失败</span>{/if}
                     </div>
                 </div>
-                <div class="easy-info-grid easy-info-grid-25">
+                <div class="easy-info-grid layui-col-xs12 layui-col-md3">
                     <div class="easy-info-grid-left">请求地址：</div>
                     <div class="easy-info-grid-right">{$log.requestUrl}</div>
                 </div>
-                <div class="easy-info-grid easy-info-grid-25">
+                <div class="easy-info-grid layui-col-xs12 layui-col-md3">
                     <div class="easy-info-grid-left">请求IP：</div>
                     <div class="easy-info-grid-right">{$log.requestIp}</div>
                 </div>
-                <div class="easy-info-grid easy-info-grid-25">
+                <div class="easy-info-grid layui-col-xs12 layui-col-md3">
                     <div class="easy-info-grid-left">操作时间：</div>
                     <div class="easy-info-grid-right">{$log.createTime}</div>
                 </div>
-                <div class="easy-info-grid easy-info-grid-25">
+                <div class="easy-info-grid layui-col-xs12 layui-col-md3">
                     <div class="easy-info-grid-left">请求耗时：</div>
                     <div class="easy-info-grid-right">{$log.costTime}秒</div>
                 </div>
-                <div class="easy-info-grid easy-info-grid-100">
+                <div class="easy-info-grid layui-col-xs12 layui-col-md12">
                     <div class="easy-info-grid-left">用户代理：</div>
                     <div class="easy-info-grid-right">{$log.userAgent}</div>
                 </div>
-                <div class="easy-info-grid easy-info-grid-100">
+                <div class="easy-info-grid layui-col-xs12 layui-col-md12">
                     <div class="easy-info-grid-left">操作信息：</div>
                     <div class="easy-info-grid-right">{$log.message}</div>
                 </div>
-                <div class="easy-info-grid easy-info-grid-100">
+                <div class="easy-info-grid layui-col-xs12 layui-col-md12">
                     <div class="easy-info-grid-left">请求参数：</div>
                     <div class="easy-info-grid-right">
                         <pre>{:json_encode(json_decode($log.params), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)}</pre>

be066d2d472dc87fa06b932c14abce4d5b626c14
liming <1628883533@qq.com>
2026-03-28 11:31:56 +0800

    .

--- application/common/repository/Repository.php
+++ application/common/repository/Repository.php
@@ -4,10 +4,212 @@
 namespace app\common\repository;
 
 
+use think\Db;
+use think\db\Query;
+use think\Paginator;
 use think\Exception;
 
-class Repository extends Dao
+use app\common\util\DateTimeUtil;
+use app\common\constant\YesnoConstant;
+
+abstract class Repository
 {
+    /**
+     * 引入分表
+     */
+    use SubTable;
+
+    /**
+     * 数据表名
+     * @var string
+     */
+    protected $name = '';
+
+    /**
+     * 表主键
+     * @var string
+     */
+    protected $tableId = 'id';
+
+    /**
+     * 获取查询器
+     * @return Query
+     */
+    public function getQuery()
+    {
+        return Db::name($this->getName());
+    }
+
+    /**
+     * 获取列表
+     * @param Wrapper $Wrapper
+     * @return Paginator
+     * @throws Exception
+     */
+    public function getPage(Wrapper $Wrapper)
+    {
+        return $this->getQuery()
+            ->where($Wrapper->getWhere())
+            ->where($Wrapper->getAndOr())
+            ->whereOr($Wrapper->getWhereOr())
+            ->field($Wrapper->getField())
+            ->group($Wrapper->getGroup())
+            ->having($Wrapper->getHaving())
+            ->order($Wrapper->getOrder())
+            ->paginate($Wrapper->getLimit());
+    }
+
+    /**
+     * 获取列表
+     * @param Wrapper $Wrapper
+     * @return array
+     * @throws Exception
+     */
+    public function getList(Wrapper $Wrapper)
+    {
+        return $this->getQuery()
+            ->where($Wrapper->getWhere())
+            ->where($Wrapper->getAndOr())
+            ->whereOr($Wrapper->getWhereOr())
+            ->page($Wrapper->getPage())
+            ->limit($Wrapper->getLimit())
+            ->field($Wrapper->getField())
+            ->group($Wrapper->getGroup())
+            ->having($Wrapper->getHaving())
+            ->order($Wrapper->getOrder())
+            ->select();
+    }
+
+    /**
+     * 获取总数
+     * @param Wrapper $Wrapper
+     * @return int
+     */
+    public function getTotal(Wrapper $Wrapper)
+    {
+        return $this->getQuery()
+            ->where($Wrapper->getWhere())
+            ->where($Wrapper->getAndOr())
+            ->whereOr($Wrapper->getWhereOr())
+            ->count();
+    }
+
+    /**
+     * 获取全部
+     * @param Wrapper $Wrapper
+     * @return array
+     * @throws Exception
+     */
+    public function getAll(Wrapper $Wrapper)
+    {
+        return $this->getQuery()
+            ->where($Wrapper->getWhere())
+            ->where($Wrapper->getAndOr())
+            ->whereOr($Wrapper->getWhereOr())
+            ->field($Wrapper->getField())
+            ->group($Wrapper->getGroup())
+            ->having($Wrapper->getHaving())
+            ->order($Wrapper->getOrder())
+            ->select();
+    }
+
+    /**
+     * 获取单个
+     * @param Wrapper $Wrapper
+     * @return array
+     * @throws Exception
+     */
+    public function getOne(Wrapper $Wrapper)
+    {
+        return $this->getQuery()
+            ->where($Wrapper->getWhere())
+            ->where($Wrapper->getAndOr())
+            ->whereOr($Wrapper->getWhereOr())
+            ->field($Wrapper->getField())
+            ->group($Wrapper->getGroup())
+            ->order($Wrapper->getOrder())
+            ->find();
+    }
+
+    /**
+     * 创建数据
+     * @param array $data
+     * @return int
+     */
+    public function createRecord(array $data = [])
+    {
+        $dateTime = DateTimeUtil::dateTime();
+
+        $data['createTime'] = $dateTime;
+        $data['updateTime'] = $dateTime;
+
+        return $this->getQuery()->insertGetId($data);
+    }
+
+    /**
+     * 批量创建
+     * @param array $dataList
+     * @return int
+     */
+    public function createAll(array $dataList = [])
+    {
+        $dateTime = DateTimeUtil::dateTime();
+
+        foreach ($dataList as &$data) {
+            $data['createTime'] = $dateTime;
+            $data['updateTime'] = $dateTime;
+        }
+
+        return $this->getQuery()->insertAll($dataList);
+    }
+
+    /**
+     * 更新数据
+     * @param Wrapper $Wrapper
+     * @param array $data
+     * @return int
+     * @throws Exception
+     */
+    public function updateRecord(Wrapper $Wrapper, array $data = [])
+    {
+        $data['updateTime'] = DateTimeUtil::dateTime();
+
+        return $this->getQuery()
+            ->where($Wrapper->getWhere())
+            ->whereOr($Wrapper->getWhereOr())
+            ->update($data);
+    }
+
+    /**
+     * 删除数据
+     * @param Wrapper $Wrapper
+     * @return int
+     * @throws Exception
+     */
+    public function deleteRecord(Wrapper $Wrapper)
+    {
+        return $this->getQuery()
+            ->where($Wrapper->getWhere())
+            ->delete();
+    }
+
+    /**
+     * 软删除数据
+     * @param Wrapper $Wrapper
+     * @return int
+     * @throws Exception
+     */
+    public function removeRecord(Wrapper $Wrapper)
+    {
+        $data['isDelete']   = YesnoConstant::Y;
+        $data['deleteTime'] = DateTimeUtil::dateTime();
+
+        return $this->getQuery()
+            ->where($Wrapper->getWhere())
+            ->where('isDelete', YesnoConstant::N)
+            ->update($data);
+    }
+
     /**
      * 通过ID获取
      * @param $id
--- application/common/repository/Wrapper.php
+++ application/common/repository/Wrapper.php
@@ -6,8 +6,62 @@ namespace app\common\repository;
 
 use Closure;
 
-class Wrapper extends Query
+class Wrapper
 {
+    /**
+     * 并联条件
+     * @var array
+     */
+    protected $where = [];
+
+    /**
+     * 或条件
+     * @var array
+     */
+    protected $whereOr = [];
+
+    /**
+     * 并联或条件
+     * @var array
+     */
+    protected $andOr = [];
+
+    /**
+     * 当前页
+     * @var int
+     */
+    protected $page = 1;
+
+    /**
+     * 限制条数
+     * @var int
+     */
+    protected $limit = 10;
+
+    /**
+     * 排序
+     * @var array
+     */
+    protected $order = [];
+
+    /**
+     * 查询字段
+     * @var array
+     */
+    protected $field = [];
+
+    /**
+     * 分组
+     * @var array
+     */
+    protected $group = [];
+
+    /**
+     * 分组筛选
+     * @var string
+     */
+    protected $having = '';
+
     /**
      * 回调条件
      * @var array
@@ -25,6 +79,231 @@ class Wrapper extends Query
      * @var Closure
      */
     protected $closureWhereOr = null;
+
+    /**
+     * 获取where
+     * @return array
+     */
+    public function getWhere()
+    {
+        return $this->where;
+    }
+
+    /**
+     * 设置where
+     * @param array $where
+     */
+    public function setWhere(array $where)
+    {
+        $this->where = $where;
+    }
+
+    /**
+     * 添加where
+     * @param $field
+     * @param $condition
+     * @param $value
+     */
+    public function addWhere($field, $condition, $value)
+    {
+        $this->where[] = [$field, $condition, $value];
+    }
+
+    /**
+     * 获取whereOr
+     * @return array
+     */
+    public function getWhereOr()
+    {
+        return $this->whereOr;
+    }
+
+    /**
+     * 设置whereOr
+     * @param array $whereOr
+     */
+    public function setWhereOr(array $whereOr)
+    {
+        $this->whereOr = $whereOr;
+    }
+
+    /**
+     * 添加whereOr
+     * @param $field
+     * @param $condition
+     * @param $value
+     */
+    public function addWhereOr($field, $condition, $value)
+    {
+        $this->whereOr[] = [$field, $condition, $value];
+    }
+
+    /**
+     * 获取andOr
+     * @return callable
+     */
+    public function getAndOr()
+    {
+        return function ($query) {
+            $query->whereOr($this->andOr);
+        };
+    }
+
+    /**
+     * 设置andOr
+     * @param array $andOr
+     */
+    public function setAndOr(array $andOr)
+    {
+        $this->andOr = $andOr;
+    }
+
+    /**
+     * 添加andOr
+     * @param $field
+     * @param $condition
+     * @param $value
+     */
+    public function addAndOr($field, $condition, $value)
+    {
+        $this->andOr[] = [$field, $condition, $value];
+    }
+
+    /**
+     * 获取page
+     * @return int
+     */
+    public function getPage()
+    {
+        return $this->page;
+    }
+
+    /**
+     * 设置分页
+     * @param int $page
+     */
+    public function setPage($page)
+    {
+        $this->page = !empty($page) ? $page : $this->page;
+    }
+
+    /**
+     * 获取limit
+     * @return int
+     */
+    public function getLimit()
+    {
+        return $this->limit;
+    }
+
+    /**
+     * 设置查询条数
+     * @param $limit
+     */
+    public function setLimit($limit)
+    {
+        $this->limit = !empty($limit) ? $limit : $this->limit;
+    }
+
+    /**
+     * 获取order
+     * @return array
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+
+    /**
+     * 设置排序
+     * @param array $order
+     */
+    public function setOrder(array $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * 添加排序
+     * @param $field
+     * @param $order
+     */
+    public function addOrder($field, $order = 'asc')
+    {
+        $this->order[$field] = $order;
+    }
+
+    /**
+     * 获取field
+     * @return array
+     */
+    public function getField()
+    {
+        return $this->field;
+    }
+
+    /**
+     * 设置field
+     * @param array $field
+     */
+    public function setField(array $field)
+    {
+        $this->field = $field;
+    }
+
+    /**
+     * 添加field
+     * @param $field
+     */
+    public function addField($field)
+    {
+        $this->field[] = $field;
+    }
+
+    /**
+     * 获取group
+     * @return array
+     */
+    public function getGroup()
+    {
+        return $this->group;
+    }
+
+    /**
+     * 设置分组
+     * @param array $group
+     */
+    public function setGroup(array $group)
+    {
+        $this->group = $group;
+    }
+
+    /**
+     * 添加分组
+     * @param $group
+     */
+    public function addGroup($group)
+    {
+        $this->group[] = $group;
+    }
+
+    /**
+     * 获取having
+     * @return string
+     */
+    public function getHaving()
+    {
+        return $this->having;
+    }
+
+    /**
+     * 设置having
+     * @param $having
+     */
+    public function setHaving($having)
+    {
+        $this->having = $having;
+    }
 
     /**
      * 设置回调
